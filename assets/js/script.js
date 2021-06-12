6202cfe6369637d7707ec0476e890ad2efef22ba
algmoreno <alg.moreno00@gmail.com>
2021-06-12 00:40:09 -0700

    daily forecast boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var searchInput = document.querySelector("#searchBox");
 var searchBtn = document.querySelector("#searchBtn"); 
 var resultsColumn = document.querySelector("#resultsColumn");
 var userSearch; 
+var dailyDiv; 
 
 var dateEl = document.createElement("h3")
 dateEl.textContent = moment().format("dddd, MMMM Do YYYY");
@@ -77,12 +78,19 @@ var displayWeather = function(data) {
         resultsContainer.append(rainyEl); 
     }
 
+    else if (condition === "Mist") {
+        var mistyEl = document.createElement("img");
+        mistyEl.setAttribute("src", "./assets/images/mist.png"); 
+        mistyEl.className = "weather-icon"; 
+        resultsContainer.append(mistyEl); 
+    }
+
     var humidityEl = document.createElement("h3")
     humidityEl.textContent = "Humidity: " + data.current.humidity + "%"; 
     humidityEl.className = "humidity-text"; 
 
     var windEl = document.createElement("h3");  
-    windEl.textContent= "Wind Speed: " + data.current.wind_speed + "mph"
+    windEl.textContent= "Wind Speed: " + data.current.wind_speed + " mph"
     windEl.className= "wind-text";
 
     var uvi = data.current.uvi;
@@ -102,11 +110,81 @@ var displayWeather = function(data) {
     uvEl.textContent = "UVI: " + uvi
     uvEl.className= "uvi-text"; 
     uvBox.append(uvEl); 
-
+    
     resultsContainer.append(tempEl); 
     resultsContainer.append(humidityEl);
     resultsContainer.append(windEl); 
     resultsContainer.append(uvBox); 
+
+    var forecastDiv = document. createElement("div");
+    forecastDiv.className = "forecast-box";
+    resultsContainer.append(forecastDiv);
+    
+    for (var i = 0; i < 5; i++) {
+
+
+        if (data.daily[i].weather[0].main === "Clear") {
+            dailyDiv = document.createElement("div");
+            dailyDiv.className = "daily-box";
+
+            var sunnyEl = document.createElement("img");
+            sunnyEl.setAttribute("src", "./assets/images/sun.png"); 
+            sunnyEl.className = "small-weather-icon"; 
+            dailyDiv.append(sunnyEl); 
+        }
+    
+        else if (data.daily[i].weather[0].main === "Clouds") {
+            dailyDiv = document.createElement("div");
+            dailyDiv.className = "daily-box";
+
+            var cloudyEl = document.createElement("img");
+            cloudyEl.setAttribute("src", "./assets/images/clouds2.png"); 
+            cloudyEl.className = "small-weather-icon"; 
+            dailyDiv.append(cloudyEl); 
+        }
+    
+        else if (data.daily[i].weather[0].main === "Rain") {
+            dailyDiv = document.createElement("div");
+            dailyDiv.className = "daily-box";
+
+            var rainyEl = document.createElement("img");
+            rainyEl.setAttribute("src", "./assets/images/rain.png"); 
+            rainyEl.className = "small-weather-icon"; 
+            dailyDiv.append(rainyEl); 
+        }
+    
+        else if (data.daily[i].weather[0].main === "Mist") {
+            dailyDiv = document.createElement("div");
+            dailyDiv.className = "daily-box";
+
+            var mistyEl = document.createElement("img");
+            mistyEl.setAttribute("src", "./assets/images/mist.png"); 
+            mistyEl.className = "small-weather-icon"; 
+            dailyDiv.append(mistyEl); 
+        }
+
+        
+
+        var highEl = document.createElement("h3");
+        highEl.textContent = "High: " + data.daily[i].temp.max  + "°F";
+        highEl.className = "forecast-text";
+    
+        var lowEl = document.createElement("h3");
+        lowEl.innerHTML = "Low: " + data.daily[i].temp.min + "°F";
+
+        lowEl.className = "forecast-text";
+
+        var dailyHumEl= document.createElement("h3");
+        dailyHumEl = "Wind Speed: " + data.daily[i].wind_speed + " mph"
+        dailyHumEl.className = "forecast-text";
+
+        
+        dailyDiv.append(highEl);
+        dailyDiv.append(lowEl); 
+        dailyDiv.append(dailyHumEl)
+        forecastDiv.append(dailyDiv); 
+    }
+
 }
 
 searchBtn.addEventListener("click", searchCity)
